e748cc77b441277708b4a710ab458af03e31c3e9
Joel Buentello <joel.buentello@yahoo.com>
2019-10-01 14:08:11 -0500

    Installed dotenv package to be able to run module

--- liriBot.js
+++ liriBot.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 
-var keys = require("keys.js");
+var keys = require("./keys.js");
 
 var axios = require("axios");
 
@@ -10,16 +10,22 @@ var Spotify = require('node-spotify-api');
 
 var fs = require("fs");
 
+var query = process.argv[3]
 
+// console.log(keys);
+
+var choose = process.argv[2];
+
+console.log(choose);
  
 
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
  
-spotify
-  .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
-  .then(function(data) {
-    console.log(data); 
-  })
-  .catch(function(err) {
-    console.error('Error occurred: ' + err); 
-  });
+// spotify
+//   .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+//   .then(function(data) {
+//     console.log(data); 
+//   })
+//   .catch(function(err) {
+//     console.error('Error occurred: ' + err); 
+//   });
