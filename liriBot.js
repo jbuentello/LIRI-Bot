db02e8b031caeac8f1372bca8a3cdd94c56e90d4
Joel Buentello <joel.buentello@yahoo.com>
2019-10-01 12:52:48 -0500

    Added global variables

--- liriBot.js
+++ liriBot.js
@@ -2,9 +2,18 @@ require("dotenv").config();
 
 var keys = require("keys.js");
 
+var axios = require("axios");
+
+var moment = require("moment");
+
 var Spotify = require('node-spotify-api');
+
+var fs = require("fs");
+
+
  
-var spotify = new Spotify(keys.spotify)
+
+var spotify = new Spotify(keys.spotify);
  
 spotify
   .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
